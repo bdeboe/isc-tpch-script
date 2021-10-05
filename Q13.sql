6eba7d6ec9db1ee3f6b6687806e9b45730b2d385
J Mark <johnmarkmorris@gmail.com>
2021-10-05 10:55:25 -0400

    Add files via upload

--- Q13.sql
+++ Q13.sql
@@ -1,7 +1,4 @@
-DROP VIEW orders_per_cust0
-;
-
-CREATE VIEW orders_per_cust0 (custkey, ordercount) as select c_custkey, count(o_orderkey) FROM %PARALLEL customer left outer join orders on c_custkey = o_custkey and o_comment not like '%%special%%requests%%' group by c_custkey
+CREATE OR REPLACE VIEW orders_per_cust0 (custkey, ordercount) as select c_custkey, count(o_orderkey) FROM %PARALLEL customer left outer join orders on c_custkey = o_custkey and o_comment not like '%%special%%requests%%' group by c_custkey
 ;
 
 SELECT ordercount, count(*) as custdist FROM %PARALLEL orders_per_cust0 group by ordercount order by custdist desc, ordercount desc
